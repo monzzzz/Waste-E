983257573cbc1c09ca6a0646497574ae1ca351cb
Elmond <elmond.pattanan@gmail.com>
2025-05-19 00:07:37 -0700

    carla downloaded in ros_container

--- docker/ros2_container/ros2_ws/build/carla_control_node/prefix_override/sitecustomize.py
+++ docker/ros2_container/ros2_ws/build/carla_control_node/prefix_override/sitecustomize.py
@@ -1,3 +1,3 @@
 import sys
 sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/root/ros2_ws/install/carla_control_node'
+sys.prefix = sys.exec_prefix = '/ros2_ws/install/carla_control_node'
